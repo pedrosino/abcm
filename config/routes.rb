8addcdc8b80b8a4ed6a989dbcf57c00b9060320e
Pedro Santos <pedrosino@gmail.com>
2016-12-28 19:35:29 -0200

    Rotas inicias, controller Home.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,21 @@
 Rails.application.routes.draw do
+  get 'home/index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'home#index'
+
+  resources :eventos
+
+  resources :noticias
+
+  resources :delegados
+
+  resources :associados
+
+  resources :galerias
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
