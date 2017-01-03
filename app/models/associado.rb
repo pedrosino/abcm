c04cd617b05d9d45d4b18017642a65e9e2537b22
Pedro Santos <pedrosino@gmail.com>
2017-01-03 13:35:56 -0200

    Devise em português.

--- app/models/associado.rb
+++ app/models/associado.rb
@@ -1,2 +1,3 @@
 class Associado < ActiveRecord::Base
+  belongs_to :usuario
 end
--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -2,5 +2,8 @@ class Usuario < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable
+
+  # has_many :inscricoes
+  has_one :associado
 end
